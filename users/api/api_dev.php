9b596758818f662bc60ed687552d974d91446a24
ITG_MonsterGeek <alexlegrem@gmail.com>
2014-07-14 19:56:18 +0200

    Correction de modifications inutiles
    Correction de modifications inutiles dans l'API et suppression d'un
    fichier de test inutile.

--- users/api/api_dev.php
+++ users/api/api_dev.php
@@ -7,14 +7,13 @@
  */
 
 
-class api_dev extends Mysqli{
-
+class api_dev{
 
     public function get_info_dev(){
 
-        include './sys/database.php';
+        include '.../../sys/database.php';
         $get_msg = "SELECT  `corps_msg` ,  `date_msg` FROM  `api_dev` WHERE  `last_msg` =  '1'";
-        $get_msg_row = $this->query($get_msg);
+        $get_msg_row = $mysqli->query($get_msg);
         $msg_row = $get_msg_row->fetch_array();
 
         $final_msg = $msg_row['corps_msg']." ".$msg_row['date_msg'];
@@ -31,13 +30,12 @@ class api_dev extends Mysqli{
     public function set_message($message,$date,$heure){
 
         include '../../sys/database.php';
-        //on passe l'ancien message à 0
 
         $update = "UPDATE  `pbeplay`.`api_dev` SET  `last_msg` =  '0' WHERE  `api_dev`.`last_msg` =1;";
-        $this->query($update);
+        $mysqli->query($update);
 
         $new_msg = "INSERT INTO `pbeplay`.`api_dev` (`corps_msg`, `date_msg`, `last_msg`) VALUES ('".$message."', '".$date." à ".$heure."', '1');";
-        $this->query($new_msg);
+        $mysqli->query($new_msg);
 
     }
 
